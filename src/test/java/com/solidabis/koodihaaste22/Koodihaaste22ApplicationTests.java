fe8d615fe6c11c17faa50e92b2456fd1010b4a58
Marko Lauronen <marko.lauronen@solidabis.com>
2022-03-25 14:36:10 +0200

    when cookie sent, no response cookie

--- src/test/java/com/solidabis/koodihaaste22/Koodihaaste22ApplicationTests.java
+++ src/test/java/com/solidabis/koodihaaste22/Koodihaaste22ApplicationTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.Cookie;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,5 +30,13 @@ class Koodihaaste22ApplicationTests {
 				.andExpect(status().isOk())
 				.andExpect(cookie().exists(VOTERID_COOKIE_NAME))
 				.andExpect(cookie().httpOnly(VOTERID_COOKIE_NAME, true));
+	}
+
+	@Test
+	void shouldNotSetVoterIdIfCookieSet() throws Exception {
+		var cookieVoterId = new Cookie(VOTERID_COOKIE_NAME, "Höttöä");
+		mockMvc.perform(get("/lounaspaikat/Kempele").cookie(cookieVoterId))
+				.andExpect(status().isOk())
+				.andExpect(cookie().doesNotExist(VOTERID_COOKIE_NAME));
 	}
 }
