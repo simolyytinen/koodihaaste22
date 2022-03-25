828d471c2856204037e951be36eaac226c959740
Marko Lauronen <marko.lauronen@solidabis.com>
2022-03-25 16:21:41 +0200

    refactor tests

--- src/test/java/com/solidabis/koodihaaste22/LounasPaikkaParserTests.java
+++ src/test/java/com/solidabis/koodihaaste22/LounasPaikkaParserTests.java
@@ -2,6 +2,7 @@ package com.solidabis.koodihaaste22;
 
 import com.google.common.io.Resources;
 import com.solidabis.koodihaaste22.lounaspaikat.LounaspaikkaParser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -10,21 +11,33 @@ import java.nio.charset.StandardCharsets;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LounasPaikkaParserTests {
+
+    private LounaspaikkaParser parser;
+
+    @BeforeEach
+    public void setup() {
+        parser = new LounaspaikkaParser();
+    }
+
     @Test
     public void shouldParseEmptyLounaspaikkaListFromEmptyDocument() {
-        var parser = new LounaspaikkaParser();
         var lounasPaikat = parser.parse("<html></html>");
         assertEquals(0, lounasPaikat.size());
     }
 
     @Test
-    public void shouldParseExampleRestaurants() throws IOException {
-        var resource = Resources.getResource("kempele.html");
-        var html = Resources.toString(resource, StandardCharsets.UTF_8);
-
-        var parser = new LounaspaikkaParser();
+    public void shouldParseExampleRestaurants() {
+        String html = getTestHtml("kempele.html");
         var lounasPaikat = parser.parse(html);
         assertEquals(3, lounasPaikat.size());
     }
 
+    private String getTestHtml(String resourceName) {
+        var resource = Resources.getResource(resourceName);
+        try {
+            return Resources.toString(resource, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("could not load resource " + resourceName, e);
+        }
+    }
 }
