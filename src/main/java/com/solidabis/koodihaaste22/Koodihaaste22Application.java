5e1b73290ad1cff3f0f2afba27f87e58b68d020d
Marko Lauronen <marko.lauronen@solidabis.com>
2022-03-25 09:54:33 +0200

    first commit

---
+++ src/main/java/com/solidabis/koodihaaste22/Koodihaaste22Application.java
@@ -0,0 +1,13 @@
+package com.solidabis.koodihaaste22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Koodihaaste22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Koodihaaste22Application.class, args);
+	}
+
+}
